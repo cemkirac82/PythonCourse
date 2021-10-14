1a4807cc673addafa5d8722d45255c1dc466bb00
cemkirac82 <56783412+cemkirac82@users.noreply.github.com>
2021-10-14 18:01:31 +0300

    Add files via upload

---
+++ QMBU472.py
@@ -0,0 +1,13 @@
+class MyStuff(object):
+
+    def __init__(self,adet):
+        self.tangerine = "And now a thousand years between"
+        self.kactane=adet
+
+    def apple(self):
+        print("I AM CLASSY APPLES!",self.kactane)
+        
+        
+cemesyalar=MyStuff(35)
+
+cemesyalar.apple()
